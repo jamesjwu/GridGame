8a75edeb4114e5213f4a0cc676c30e8126c335ab
James Wu <jameswu@cmu.edu>
2014-10-07 13:38:26 -0400

    Added basic grid functionality

--- GridGame/GameModel.swift
+++ GridGame/GameModel.swift
@@ -7,7 +7,63 @@
 //
 
 import Foundation
+import UIKit
+
+
 /// Taken from swift-2048
+/// An enum representing directions supported by the game model.
+enum MoveDirection {
+  case Up
+  case Down
+  case Left
+  case Right
+}
+
+/// An enum representing a movement command issued by the view controller as the result of the user swiping.
+struct MoveCommand {
+  var direction: MoveDirection
+  var completion: (Bool) -> ()
+  init(d: MoveDirection, c: (Bool) -> ()) {
+    direction = d
+    completion = c
+  }
+}
+
+enum GridObject {
+  case Empty
+  case Ball //there's a ball here
+  case Hole //location to put the Ball
+  case Block //a wall blocking the ball
+}
+
+protocol GameModelProtocol : class {
+  func scoreChanged(score: Int)
+  func moveBall(from: (Int, Int), to: (Int, Int), value: Int)
+}
+
+
+class GameModel : NSObject {
+  let dimension: Int
+  let threshold: Int
+  
+  let delegate = GameModelProtocol
+  
+  var score: Int = 0 {
+    didSet {
+      delegate.scoreChanged(score)
+    }
+  }
+  
+  var gameboard = SquareGameBoard<TileObject>
+
+
+}
+
+
+
+
+
+
 
 /// A struct representing a square gameboard. Because this struct uses generics, it could conceivably be used to
 /// represent state for many other games without modification.
